141cc9679ebacf229d95453816f457205adacd2e
Dan-McDermott <42184164+Dan-McDermott@users.noreply.github.com>
2025-07-02 14:25:05 -0400

    added jambonz client variabes
    added sid, api key, webhook secret, outbound sip host for jambonz

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name: 'tcxc-registrar',
+    name: 'medbizz-registrar',
     script: 'app.js',
     instance_var: 'INSTANCE_ID',
     exec_mode: 'fork',
@@ -10,13 +10,13 @@ module.exports = {
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production',
-      LOGLEVEL: 'info',
+      LOGLEVEL: 'trace',
       HTTP_PORT: 80,
-      JAMBONZ_ACCOUNT_SID: '<your-account-sid>',
-      JAMBONZ_API_KEY: '<your-api-key>',
+      JAMBONZ_ACCOUNT_SID: 'b4bf890a-043b-4c69-a51e-5dbc2fdcdb56',
+      JAMBONZ_API_KEY: 'b95c8b44-e03d-44a5-8b68-f8da488a3ab2',
       JAMBONZ_REST_API_BASE_URL: 'https://api.jambonz.us/v1',
-      WEBHOOK_SECRET: 'your-webhook-secret',
-      OUTBOUND_SIP_HOST: 'sip01.TelecomsXChange.com'
+      WEBHOOK_SECRET: 'wh_secret_qZNgsqZ7WJN6x6JnsR4sUf',
+      OUTBOUND_SIP_HOST: 'afterours.sip.jambonz.cloud'
     }
   }]
 };
