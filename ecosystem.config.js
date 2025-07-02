add7d48a72475e7d9d57a26cd09b82b03999d384
Dan-McDermott <42184164+Dan-McDermott@users.noreply.github.com>
2025-07-02 14:55:55 -0400

    fix: update JAMBONZ_REST_API_BASE_URL in ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
       HTTP_PORT: 80,
       JAMBONZ_ACCOUNT_SID: 'b4bf890a-043b-4c69-a51e-5dbc2fdcdb56',
       JAMBONZ_API_KEY: 'b95c8b44-e03d-44a5-8b68-f8da488a3ab2',
-      JAMBONZ_REST_API_BASE_URL: 'https://api.jambonz.us/v1',
+      JAMBONZ_REST_API_BASE_URL: 'https://jambonz.cloud/api',
       WEBHOOK_SECRET: 'wh_secret_qZNgsqZ7WJN6x6JnsR4sUf',
       OUTBOUND_SIP_HOST: 'afterours.sip.jambonz.cloud'
     }
